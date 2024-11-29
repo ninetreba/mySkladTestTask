ce18c32fc829a4f3eae83c28ffa13af55547eede
nikol <nikolay.netreba@mail.ru>
2024-11-29 09:19:46 +0300

    t4

--- src/main/java/com/example/myskladtesttask/repository/ProductRepository.java
+++ src/main/java/com/example/myskladtesttask/repository/ProductRepository.java
@@ -2,6 +2,32 @@ package com.example.myskladtesttask.repository;
 
 import com.example.myskladtesttask.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    List<Product> findAllByName(String name);
+    List<Product> findByNameContaining(String nameLike);
+
+    List<Product> findByPrice(int price);
+    List<Product> findByPriceGreaterThan(int price);
+    List<Product> findByPriceLessThan(int price);
+
+    List<Product>findAllByisAvailableTrue();
+    List<Product>findAllByisAvailableFalse();
+
+    List<Product> findByOrderByPriceAsc();
+    List<Product> findByOrderByNameAsc();
+
+
+
+    List<Product>findByPriceGreaterThanOrderByNameDesc(int price);
+
+    @Query(value = "SELECT * FROM product p where p.name = ?1" +
+            " ORDER BY p.price LIMIT ?2", nativeQuery = true)
+    List<Product> findByNameSortLimit(String name, int limit);
+
 }
